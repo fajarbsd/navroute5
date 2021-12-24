33e477b1c1077acb5e101f94e1414087e144a797
fajarbsd <fajar.fakhran@apsschef.com>
2021-12-24 09:34:39 +0700

    latihan5

---
+++ android/app/src/main/kotlin/com/example/navroute5/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.navroute5
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    home: new Halsatu(),
+    title: "Navigasi",
+    routes: <String, WidgetBuilder>{
+      '/Halsatu': (BuildContext context) => new Halsatu(),
+      '/Haldua': (BuildContext context) => new Haldua(),
+    },
+  ));
+}
+
+class Halsatu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text("MUSIC"),
+        ),
+        body: new Center(
+            child: new IconButton(
+          icon: new Icon(
+            Icons.headset,
+            size: 50.0,
+            color: Colors.grey,
+          ),
+          onPressed: () {
+            Navigator.pushNamed(context, '/Haldua');
+          },
+        )));
+  }
+}
+
+class Haldua extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text("SPEAKER"),
+        ),
+        body: new Center(
+            child: new IconButton(
+          icon: new Icon(
+            Icons.speaker,
+            size: 50.0,
+            color: Colors.green,
+          ),
+          onPressed: () {
+            Navigator.pushNamed(context, '/Halsatu');
+          },
+        )));
+  }
+}
